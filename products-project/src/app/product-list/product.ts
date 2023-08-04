2a1f22ba19ce1e2480526820be21eef9285fc200
deeptha28 <71130912+deeptha28@users.noreply.github.com>
2023-08-04 13:12:11 +0530

    Delete products-project directory

--- products-project/src/app/product-list/product.ts
+++
@@ -1,8 +0,0 @@
-export class Product {
-    constructor(public productId: number,
-        public productName: string,
-        public productPrice: number,
-        public isAvailable: boolean) {
-        
-        }
-}
